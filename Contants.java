76532a432427de7a1c099e12b10222056ab77aba
lkx8610 <1842196447@qq.com>
2018-01-08 14:19:58 +0800

    dd

---
+++ Contants.java
@@ -0,0 +1,14 @@
+package com.test.demo.Utility;
+
+public class Contants {
+	public static final int keycolumn = 0;//第一�?
+	public static final int column = 1;//第二�?
+	public static final String path = System.getProperty("user.dir")
+			+ "\\src\\testdata\\";
+	public static final String filename = "Exceldata.xlsx";
+	public static final String sheetname = "Login";
+	public static final String csvFileName = "Exceldata.csv";
+	public static final String ObjectFile = "ObjectRepository.csv";
+	public static final String ieDriverServer = "IEDriverServer.exe";
+	
+}
